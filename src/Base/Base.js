756fb457bbe080c9f67ac54420985a7fca341741
SairamGurunathan <ram_carwash@yahoo.com>
2023-10-06 12:01:55 +0530

    removed unwanted lines

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,55 +1,3 @@
-// import React from "react";
-
-
-// export default function Base({children}) {
-//   return (
-//     <>
-//       <header>
-//         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//           <div className="container-fluid">
-//             <a className="navbar-brand" href="/">
-//               Zen School
-//             </a>
-//             <button
-//               className="navbar-toggler"
-//               type="button"
-//               data-bs-toggle="collapse"
-//               data-bs-target="#navbarNav"
-//               aria-controls="navbarNav"
-//               aria-expanded="false"
-//               aria-label="Toggle navigation"
-//             >
-//               <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarNav">
-//               <ul className="navbar-nav ms-auto">
-//                 <li className="nav-item">
-//                   <a className="nav-link active" aria-current="page" href="/">
-//                     Home
-//                   </a>
-//                 </li>
-//                 <li className="nav-item">
-//                   <a className="nav-link" href="/student">
-//                     Student
-//                   </a>
-//                 </li>
-//                 <li className="nav-item">
-//                   <a className="nav-link" href="/teacher">
-//                     Teacher
-//                   </a>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </nav>
-//       </header>
-//       <div className="container"> 
-//         {children}
-//       </div>
-//       <footer></footer>
-//     </>
-//   );
-// }
 import React from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
@@ -57,10 +5,7 @@ import { useHistory } from "react-router-dom";
 export default function Base({ children }) {
   const history = useHistory();
   const handleNavigation = (path) => {
-  
-    
     history.push(path);
-  
   };
 
   return (
